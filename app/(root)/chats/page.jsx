56429d094d016625af55d4d1ef124daa2c71190a
kokoball <kbp4154@naver.com>
2024-01-28 21:46:28 +0900

    ChatList, Contacts 컴포넌트 추가

--- app/(root)/chats/page.jsx
+++ app/(root)/chats/page.jsx
@@ -1,10 +1,17 @@
-"use client";
-
-import { useSession } from "next-auth/react";
+import ChatList from "@components/ChatList";
+import Contacts from "@components/Contacts";
 
 const Chats = () => {
-  const { data: session } = useSession();
-  return <div>Chats</div>;
+  return (
+    <div className="main-container">
+      <div className="w-1/3 max-lg:w-1/2 max-md:w-full">
+        <ChatList />
+      </div>
+      <div className="w-2/3 max-lg:w-1/2 max-md:hidden">
+        <Contacts />
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
